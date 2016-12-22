e43d1963f005a494b935a2f4835e61a808ba5dd8
zhouwei <867571123@qq.com>
2016-12-22 23:46:43 +0800

    添加各种队列操作方式

--- ConcurrencyDemo/ViewController.swift
+++ ConcurrencyDemo/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-let imageURLs = ["http://www.planetware.com/photos-large/F/france-paris-eiffel-tower.jpg", "http://adriatic-lines.com/wp-content/uploads/2015/04/canal-of-Venice.jpg", "http://algoos.com/wp-content/uploads/2015/08/ireland-02.jpg", "http://bdo.se/wp-content/uploads/2014/01/Stockholm1.jpg"]
+let imageURLs = ["http://7xqacx.com1.z0.glb.clouddn.com/1.jpg", "http://7xqacx.com1.z0.glb.clouddn.com/2.jpg", "http://7xqacx.com1.z0.glb.clouddn.com/3.jpg", "http://7xqacx.com1.z0.glb.clouddn.com/4.jpg"]
 
 class Downloader {
     
@@ -41,21 +41,144 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+//    @IBAction func didClickOnStart(_ sender: AnyObject) {
+//        
+//        let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+//        self.imageView1.image = img1
+//        
+//        let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+//        self.imageView2.image = img2
+//        
+//        let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+//        self.imageView3.image = img3
+//        
+//        let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+//        self.imageView4.image = img4
+//        
+//    }
+
+      /// global queue
+//    @IBAction func didClickOnStart(_ sender: AnyObject) {
+//        let queue = DispatchQueue.global(qos: .default)
+//        let main = DispatchQueue.main
+//        
+//        queue.async {
+//            let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+//            main.async {
+//                self.imageView1.image = img1
+//            }
+//        }
+//        
+//        queue.async {
+//            let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+//            main.async {
+//                self.imageView2.image = img2
+//            }
+//        }
+//        
+//        queue.async {
+//            let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+//            main.async {
+//                self.imageView3.image = img3
+//            }
+//        }
+//        
+//        queue.async {
+//            let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+//            main.async {
+//                self.imageView4.image = img4
+//            }
+//        }
+//    }
+    
+    /// Serial Queue
+//    @IBAction func didClickOnStart(_ sender: AnyObject) {
+//        let serialQueue = DispatchQueue(label: "serialQueue1")
+//        let main = DispatchQueue.main
+//        
+//        serialQueue.async {
+//            let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+//            main.async {
+//                self.imageView1.image = img1
+//            }
+//        }
+//        
+//        serialQueue.async {
+//            let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+//            main.async {
+//                self.imageView2.image = img2
+//            }
+//        }
+//        
+//        serialQueue.async {
+//            let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+//            main.async {
+//                self.imageView3.image = img3
+//            }
+//        }
+//        
+//        serialQueue.async {
+//            let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+//            main.async {
+//                self.imageView4.image = img4
+//            }
+//        }
+//    }
+
+    /// NSOperationQueue
     @IBAction func didClickOnStart(_ sender: AnyObject) {
+        let queue = OperationQueue()
+        let main = OperationQueue.main
         
-        let img1 = Downloader.downloadImageWithURL(imageURLs[0])
-        self.imageView1.image = img1
+        let operation1 = BlockOperation(block: {
+            let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+            main.addOperation({
+                self.imageView1.image = img1
+            })
+        })
+        operation1.completionBlock = {
+            print("Operation 1 completed")
+        }
+        queue.addOperation(operation1)
         
-        let img2 = Downloader.downloadImageWithURL(imageURLs[1])
-        self.imageView2.image = img2
         
-        let img3 = Downloader.downloadImageWithURL(imageURLs[2])
-        self.imageView3.image = img3
+        let operation2 = BlockOperation(block: {
+            let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+            main.addOperation({
+                self.imageView2.image = img2
+            })
+        })
+        operation2.completionBlock = {
+            print("Operation 2 completed")
+        }
+        queue.addOperation(operation2)
         
-        let img4 = Downloader.downloadImageWithURL(imageURLs[3])
-        self.imageView4.image = img4
+        
+        let operation3 = BlockOperation(block: {
+            let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+            main.addOperation({
+                self.imageView3.image = img3
+            })
+        })
+        operation3.completionBlock = {
+            print("Operation 3 completed")
+        }
+        queue.addOperation(operation3)
+        
+        
+        let operation4 = BlockOperation(block: {
+            let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+            main.addOperation({
+                self.imageView4.image = img4
+            })
+        })
+        operation4.completionBlock = {
+            print("Operation 4 completed")
+        }
+        queue.addOperation(operation4)
         
     }
+    
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         
         self.sliderValueLabel.text = "\(sender.value * 100.0)"
